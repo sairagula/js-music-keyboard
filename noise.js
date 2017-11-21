5452c1e7f37800fa0db90861545b234bfd4e174b
Sairagul Abdukhalieva <abdukhalieva@gmail.com>
2017-11-20 21:41:13 -0800

    Works

--- noise.js
+++ noise.js
@@ -1,3 +1,20 @@
+
+
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function() {
+    let letter = $(this).html();
+    let audioId = ('#' + letter +'Audio');
+
+    $(audioId).get(0).currentTime = 0;
+    $(audioId).get(0).play();
+  });
 });
+
+
+// $(document).ready( function() {
+//   $('.instrument').on('click', 'button', function() {
+//     let x = $('#cAudio');
+//     x.get(0).currentTime = 0;
+//     x.get(0).play();
+//   });
+// });
